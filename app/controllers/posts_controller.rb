80a4416459eb04b2b0d12dd728ae27a9b84ee614
fuma-pooh <fuma.kido@di-v.co.jp>
2020-07-20 11:52:36 +0900

    既読機能実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,6 +12,9 @@ class PostsController < ApplicationController
   end
 
   def checked
+
+    # binding.pry
+
     post = Post.find(params[:id])
     if post.checked
       post.update(checked: false)
